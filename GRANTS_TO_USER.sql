9cd05b33b10512b1aa4aebc793eba3f5a30a2e24
jairx <jairblz@gmail.com>
2018-11-24 17:07:25 -0200

    correcao de grants

--- GRANTS_TO_USER.sql
+++ GRANTS_TO_USER.sql
@@ -1,19 +1,19 @@
-create user teamx@localhost identified by 'Copa2018';
+create user jairneto_teamx@localhost identified by 'Copa2018';
 
-grant create, drop, delete, insert, select, update on bolanarede.tipo_falta to teamx@localhost;
+grant create, drop, delete, insert, select, update on jairneto_bolanarede.tipo_falta to jairneto_teamx@localhost;
 
-grant create, drop, delete, insert, select, update on bolanarede.falta to teamx@localhost;
+grant create, drop, delete, insert, select, update on jairneto_bolanarede.falta to jairneto_teamx@localhost;
 
-grant create, drop, delete, insert, select, update on bolanarede.jogo to teamx@localhost;
+grant create, drop, delete, insert, select, update on jairneto_bolanarede.jogo to jairneto_teamx@localhost;
 
-grant create, drop, delete, insert, select, update on bolanarede.campeonato to teamx@localhost;
+grant create, drop, delete, insert, select, update on jairneto_bolanarede.campeonato to jairneto_teamx@localhost;
 
-grant create, drop, delete, insert, select, update on bolanarede.colocacao to teamx@localhost;
+grant create, drop, delete, insert, select, update on jairneto_bolanarede.colocacao to jairneto_teamx@localhost;
 
-grant create, drop, delete, insert, select, update on bolanarede.gol to teamx@localhost;
+grant create, drop, delete, insert, select, update on jairneto_bolanarede.gol to jairneto_teamx@localhost;
 
-grant create, drop, delete, insert, select, update on bolanarede.selecao to teamx@localhost;
+grant create, drop, delete, insert, select, update on jairneto_bolanarede.selecao to jairneto_teamx@localhost;
 
-grant create, drop, delete, insert, select, update on bolanarede.time_campeonato to teamx@localhost;
+grant create, drop, delete, insert, select, update on jairneto_bolanarede.time_campeonato to jairneto_teamx@localhost;
 
 flush privileges;
