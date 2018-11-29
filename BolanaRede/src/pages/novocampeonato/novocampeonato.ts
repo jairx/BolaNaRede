c25d731f03d126a7bb37af55ed71c1361e6c1aa6
jairx <jairblz@gmail.com>
2018-11-29 15:47:48 -0200

    copia de cadastro de selecao para campeonatos

--- BolanaRede/src/pages/novocampeonato/novocampeonato.ts
+++ BolanaRede/src/pages/novocampeonato/novocampeonato.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { ServidorProvider } from '../../providers/servidor/servidor';
+import { map } from 'rxjs/operators';
+import { Http } from '@angular/http';
 
 /**
  * Generated class for the NovocampeonatoPage page.
@@ -15,11 +18,58 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class NovocampeonatoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  nomeCampeonato: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              public alertCtrl: AlertController, public servidor: ServidorProvider, public http: Http) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad NovocampeonatoPage');
+  }
+
+  cadastrar() {
+    if(this.nomeCampeonato == '' || this.nomeCampeonato == undefined) {
+
+        let alert = this.alertCtrl.create({
+
+          title: 'Atenção',
+          message: 'Preencha todos os campos!',
+          buttons: ['OK']
+
+        })
+
+        alert.present();
+
+    }else{
+
+        this.http.get(this.servidor.urlGet()+'cadastrarCampeonato.php?nomeCampeonato='+this.nomeCampeonato)
+                      .pipe(map( res => res.json()))
+                        .subscribe(
+
+                          dados => {
+
+                            if(dados.valida.teste==1) {
+
+                              let alert = this.alertCtrl.create({
+
+                                title: 'Sucesso',
+                                message: 'Campeonato cadastrada.',
+                                buttons: ['OK']
+
+                              })
+
+                              alert.present();
+
+                              this.navCtrl.setRoot('CampeonatosPage');
+
+                            }
+
+                          }
+
+                        )
+
+      }
   }
 
 }
